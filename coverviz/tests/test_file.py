528c073c6791e5acf80e9625f557e4e96f42ccca
Loic Diridollou <loic.diridollou@gmail.com>
2023-02-11 17:51:38 -0800

    fixing lint

--- coverviz/tests/test_file.py
+++ coverviz/tests/test_file.py
@@ -1,7 +1,21 @@
 """Test module for file.py."""
+import json
+from pathlib import Path
+
 import coverviz.file as cf
 
 
-def test_load_json():
+def test_load_json(tmp_path: Path):
     """Testing file.load_json method."""
-    assert True
+    tmp_file = tmp_path / "test_file.json"
+    dictionary = {
+        "name": "test",
+        "rollno": 56,
+        "phonenumber": "5376770500"
+    }
+    json_object = json.dumps(dictionary, indent=4)
+
+    with tmp_file.open(mode="w") as outfile:
+        outfile.write(json_object)
+
+    cf.load_json(tmp_file)
