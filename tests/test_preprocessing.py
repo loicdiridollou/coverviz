a2aaa3131b2801aba9bc832320ae0c745f085de4
Loic Diridollou <loic.diridollou@gmail.com>
2023-02-19 11:30:06 -0800

    Updating tests

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -15,28 +15,28 @@ def test_load_json():
 
 def test_clean_up_json():
     """Test cleaning up json."""
-    input_js = {'files': {'lib/module/file.py': {'summary': {'covered_lines': 12,
-                                                             'num_statements': 14,
-                                                             'excluded_lines': 0}},
-                          'lib/module/__init__.py': {'summary': {'covered_lines': 10,
-                                                                 'num_statements': 10,
-                                                                 'excluded_lines': 0}},
-                          'lib/tests/test_file.py': {'summary': {'covered_lines': 6,
-                                                                 'num_statements': 12,
-                                                                 'excluded_lines': 0}}}}
+    input_js = {"files": {"lib/module/file.py": {"summary": {"covered_lines": 12,
+                                                             "num_statements": 14,
+                                                             "excluded_lines": 0}},
+                          "lib/module/__init__.py": {"summary": {"covered_lines": 10,
+                                                                 "num_statements": 10,
+                                                                 "excluded_lines": 0}},
+                          "lib/tests/test_file.py": {"summary": {"covered_lines": 6,
+                                                                 "num_statements": 12,
+                                                                 "excluded_lines": 0}}}}
     actual = cvp.clean_up_json(input_js)
-    expected = {'lib': {'module': {'file.py': [12, 14], '__init__.py': [10, 10]},
-                        'tests': {'test_file.py': [6, 12]}}}
+    expected = {"lib": {"module": {"file.py": [12, 14], "__init__.py": [10, 10]},
+                        "tests": {"test_file.py": [6, 12]}}}
     assert actual == expected
 
 
 def test_identify_modules():
     """Test module identification and summary of coverage."""
-    input_dic = {'lib': {'module': {'file.py': [12, 14], '__init__.py': [10, 10]},
-                         'tests': {'test_file.py': [6, 12]}}}
+    input_dic = {"lib": {"module": {"file.py": [12, 14], "__init__.py": [10, 10]},
+                         "tests": {"test_file.py": [6, 12]}}}
     actual = cvp.identify_modules(input_dic, "")
-    expected = [['/lib', [22, 24]], ['/lib/module', [22, 24]],
-                ['/lib/module/file.py', [12, 14]], ['/lib/module/__init__.py', [10, 10]]]
+    expected = [["/lib", [22, 24]], ["/lib/module", [22, 24]],
+                ["/lib/module/file.py", [12, 14]], ["/lib/module/__init__.py", [10, 10]]]
     assert actual == expected
 
 
